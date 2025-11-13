eff1a53e80aab164ffe227027eb7dc02f0d937e2
MariaRadchuk <mradchuk_15@ukr.net>
2025-11-13 19:15:19 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'public', // ← Все в public/
-  
+  root: '.',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
   },
-  base: '/ITschool/',
 });
